70e426861927cd345748592d7ce9331148754a6d
aditrizky <aditya10355@gmail.com>
2022-06-14 22:34:28 +0700

    add update profile

---
+++ app/src/main/java/com/binar/secondhand/ui/account/editaccount/EditAccountFragment.kt
@@ -0,0 +1,17 @@
+package com.binar.secondhand.ui.account.editaccount
+
+
+import android.view.View
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.binar.secondhand.R
+import com.binar.secondhand.base.BaseFragment
+import com.binar.secondhand.databinding.FragmentEditAccountBinding
+
+
+class EditAccountFragment : BaseFragment(R.layout.fragment_edit_account) {
+ private val binding: FragmentEditAccountBinding by viewBinding()
+
+ override var bottomNavigationViewVisibility = View.GONE
+
+
+}
