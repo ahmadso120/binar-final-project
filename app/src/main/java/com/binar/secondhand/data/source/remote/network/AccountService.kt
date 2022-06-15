842997e977fb72959e35bc21b046ae1c0b9c4c09
aditrizky <aditya10355@gmail.com>
2022-06-15 11:13:51 +0700

    edit account api service

--- app/src/main/java/com/binar/secondhand/data/source/remote/network/AccountService.kt
+++ app/src/main/java/com/binar/secondhand/data/source/remote/network/AccountService.kt
@@ -1,10 +1,12 @@
 package com.binar.secondhand.data.source.remote.network
 
+import com.binar.secondhand.data.source.remote.response.GetAccountResponse
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface AccountService {
 
     @GET("auth/user/1")
-    suspend fun getAccount()
+    suspend fun getAccount(): Response<GetAccountResponse>
 
 }
