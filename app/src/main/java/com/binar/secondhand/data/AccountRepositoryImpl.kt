d1184b5b7c0c8d5c3fd98faba35afdd9baf86607
aditrizky <aditya10355@gmail.com>
2022-06-15 11:56:11 +0700

    create Account Repository

---
+++ app/src/main/java/com/binar/secondhand/data/AccountRepositoryImpl.kt
@@ -0,0 +1,47 @@
+package com.binar.secondhand.data
+
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import com.binar.secondhand.data.source.remote.AccountRemoteDataSource
+import com.binar.secondhand.data.source.remote.response.GetAccountResponse
+import com.binar.secondhand.utils.loge
+import kotlinx.coroutines.Dispatchers
+import org.json.JSONObject
+
+
+interface AccountRepository {
+    fun getAccount(): LiveData<Result<GetAccountResponse>>
+}
+
+class AccountRepositoryImpl(
+    private val accountRemoteDataSource: AccountRemoteDataSource
+) : AccountRepository {
+    override fun getAccount(): LiveData<Result<GetAccountResponse>> =
+        liveData(Dispatchers.IO) {
+            emit(Result.Loading)
+            try {
+                val response = accountRemoteDataSource.getAccount()
+
+                if (response.isSuccessful){
+                    val data = response.body()
+                    data?.let {
+                        emit(Result.Success(it))
+                    }
+                }else{
+                    loge("getAccount()=> Request error")
+                    val error = response.errorBody()?.string()
+                    if (error != null){
+                        val jsonObject = JSONObject(error)
+                        val message = jsonObject.getString("message")
+                        emit(Result.Error(null, message))
+                    }
+                }
+            } catch (e: Exception){
+                loge("getAccount()=> ${e.message}")
+                emit(Result.Error(null, "Something went wrong"))
+            }
+        }
+
+
+}
