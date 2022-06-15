35b51aae2a84072a04f9e64f67ae4c95e1dd9f3a
aditrizky <aditya10355@gmail.com>
2022-06-15 12:33:41 +0700

    create account setting viewmodel

---
+++ app/src/main/java/com/binar/secondhand/ui/account/editaccount/EditAccountViewModel.kt
@@ -0,0 +1,19 @@
+package com.binar.secondhand.ui.account.editaccount
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.binar.secondhand.data.AccountRepository
+import com.binar.secondhand.data.Result
+import com.binar.secondhand.data.source.remote.response.GetAccountResponse
+
+class EditAccountViewModel(
+    private val accountRepository: AccountRepository
+) : ViewModel() {
+
+
+    private fun getAccount(): LiveData<Result<GetAccountResponse>> {
+        return accountRepository.getAccount()
+    }
+
+
+}
