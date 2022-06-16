fe16697c8e23300f8bc049cf3ceca9680c91e5a8
valentinetrihandayani-07 <you@example.com>
2022-06-16 17:40:39 +0700

    update

--- app/src/main/java/com/binar/secondhand/ui/account/AccountFragment.kt
+++ app/src/main/java/com/binar/secondhand/ui/account/AccountFragment.kt
@@ -11,19 +11,44 @@ import com.binar.secondhand.R
 import com.binar.secondhand.base.BaseFragment
 import com.binar.secondhand.databinding.FragmentAccountBinding
 import com.binar.secondhand.databinding.FragmentHomeBinding
+import com.binar.secondhand.storage.AppLocalData
+import com.binar.secondhand.utils.LogoutProcess
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class AccountFragment : BaseFragment(R.layout.fragment_account) {
 
-  /*  override var btmNavigationViewVisibilty = View.VISIBLE*/
     private val binding: FragmentAccountBinding by viewBinding()
+    override var bottomNavigationViewVisibility = View.GONE
+
 
     private val viewModel by viewModel<AccountViewModel>()
+    private val appLocalData: AppLocalData by inject()
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        observeUI()
 
+
+
+        binding.imgCam.setOnClickListener {
+            val product = getItem()
+
+
+            if (!response.data.imageUrl.isNullOrEmpty) {
+                binding.imgCam.loadPhotoUrl(response.data.imageUrl)
+            } else {
+            // ambil dari drawable
+                R.drawable.imguser
+            }
+
+
+
+
+
+        }
         binding.icEdit.setOnClickListener {
             /*findNavController().navigate(R.id.)
 */
@@ -31,11 +56,20 @@ class AccountFragment : BaseFragment(R.layout.fragment_account) {
         binding.icSettings.setOnClickListener {
 
         }
-        binding.icLogout.setOnClickListener {
-            findNavController().navigate(R.id.action_accountFragment_to_loginFragment)
-        }
+/*        binding.icLogout.setOnClickListener {
+            LogoutProcess.execute(appLocalData, binding)        }*/
     }
 
+    private fun observeUI() {
+        binding.icLogout.setOnClickListener {
+            LogoutProcess.execute(appLocalData, binding)
+        }
+
+
+
+    }
+
+}
 
 
 }
--- app/src/main/java/com/binar/secondhand/ui/account/AccountViewModel.kt
+++ app/src/main/java/com/binar/secondhand/ui/account/AccountViewModel.kt
@@ -1,8 +1,16 @@
 package com.binar.secondhand.ui.account
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.binar.secondhand.data.AuthRepository
+import com.binar.secondhand.data.Result
 
-class AccountViewModel (): ViewModel(){
-
+/*class AccountViewModel (private  val accountRepository: AccountRepository): ViewModel(){
+    fun getAccount():LiveData<Result<AccountResponse>>{
+        return accountRepository.getAccount()
+    }*/
+    class AccountViewModel (): ViewModel(){
+        /*fun getAccount():LiveData<Result<AccountResponse>>{
+            return accountRepository.getAccount()*/
+   /*     }*/
 }
