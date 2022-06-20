2739f77c98f693cbe6d14c13879daeeae83b9130
19124-rifaldi <84621245+19124-rifaldi@users.noreply.github.com>
2022-06-20 19:43:58 +0700

    rename request

--- app/src/main/java/com/binar/secondhand/ui/account/accountsetting/AccountSettingFragment.kt
+++ app/src/main/java/com/binar/secondhand/ui/account/accountsetting/AccountSettingFragment.kt
@@ -2,16 +2,13 @@ package com.binar.secondhand.ui.account.accountsetting
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.binar.secondhand.R
 import com.binar.secondhand.base.BaseFragment
 import com.binar.secondhand.data.Result
-import com.binar.secondhand.data.source.remote.request.AccountReq
+import com.binar.secondhand.data.source.remote.request.AccountSettingRequest
 import com.binar.secondhand.databinding.FragmentAccountSettingBinding
 import com.binar.secondhand.storage.AppLocalData
-import com.binar.secondhand.storage.UserLoggedIn
 import com.binar.secondhand.utils.LogoutProcess
 import com.binar.secondhand.utils.showShortSnackbar
 import org.koin.android.ext.android.inject
@@ -53,13 +50,10 @@ class AccountSettingFragment : BaseFragment(R.layout.fragment_account_setting) {
 
         binding.apply {
             button.setOnClickListener{
-                val accReq = AccountReq(
+                val accReq = AccountSettingRequest(
                     email =emailEdt.text.toString(),
                     password = passwordEdt.text.toString(),
-                    phone_number = null,
-                    address = null,
-                    image = null,
-                    full_name = null
+                   
                 )
                 viewModel.getDataChange(accReq)
 
