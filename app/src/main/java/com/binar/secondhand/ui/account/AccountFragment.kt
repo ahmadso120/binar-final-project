267ce80d73f992a1eb9cdd2fab7466783a12a8e1
valentinetrihandayani-07 <you@example.com>
2022-06-21 20:47:45 +0700

    updated ui

--- app/src/main/java/com/binar/secondhand/ui/account/AccountFragment.kt
+++ app/src/main/java/com/binar/secondhand/ui/account/AccountFragment.kt
@@ -1,23 +1,24 @@
 package com.binar.secondhand.ui.account
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.AdapterView
 import androidx.navigation.fragment.findNavController
+
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.binar.secondhand.R
 import com.binar.secondhand.base.BaseFragment
+
 import com.binar.secondhand.data.Result
+
 import com.binar.secondhand.databinding.FragmentAccountBinding
 
-
-import com.binar.secondhand.databinding.FragmentHomeBinding
 import com.binar.secondhand.storage.AppLocalData
 import com.binar.secondhand.ui.account.editaccount.EditAccountViewModel
 import com.binar.secondhand.utils.LogoutProcess
 import com.binar.secondhand.utils.loadPhotoUrl
+import kotlinx.coroutines.NonDisposableHandle.parent
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import java.io.File
@@ -26,7 +27,7 @@ import java.io.File
 class AccountFragment : BaseFragment(R.layout.fragment_account) {
     private var getFile: File? = null
     private val binding: FragmentAccountBinding by viewBinding()
-    override var bottomNavigationViewVisibility = View.GONE
+    override var bottomNavigationViewVisibility = View.VISIBLE
 
 
     private val viewModel by viewModel<EditAccountViewModel>()
@@ -36,55 +37,71 @@ class AccountFragment : BaseFragment(R.layout.fragment_account) {
         super.onViewCreated(view, savedInstanceState)
         observeUI()
 
+        var listView = binding.listview
+        var menuAccount: ArrayList<MenuAccount> = ArrayList()
+        menuAccount.add(MenuAccount("Ubah Akun", R.drawable.ic_fi_edit))
+        menuAccount.add(MenuAccount("Ubah Password", R.drawable.ic_fi_settings))
+        menuAccount.add(MenuAccount("Keluar", R.drawable.ic_fi_log_out))
+
+        listView.adapter = CustomAdapterAccount(requireContext(), menuAccount)
+        listView.setOnItemClickListener { AdapterView, View, position, id ->
+            when (position) {
+                0 -> {
+                    findNavController().navigate(R.id.action_accountFragment_to_editAccountFragment)
+                }
+                1 -> {
+                    /*     findNavController().navigate(R.id.action)*/
+                }
+                2 -> {
+                    LogoutProcess.execute(appLocalData, binding)
+                }
+
+        }
+    }
+}
+/*
         binding.icEdit.setOnClickListener {
             findNavController().navigate(R.id.action_accountFragment_to_editAccountFragment)
 
         }
         binding.icSettings.setOnClickListener {
-    /*        findNavController()*/
 
+            findNavController().navigate(R.id.action_accountFragment_to_accountSettingFragment)
 
         }
- /*       binding.icLogout.setOnClickListener {
-            LogoutProcess.execute(appLocalData, binding)        }*/
-    }
+        }*/
 
 
-    private fun observeUI() {
-        viewModel.getAccount().observe(viewLifecycleOwner) {
-            when (it) {
-                is Result.Error -> {
 
-                }
-                Result.Loading -> {
 
-                }
-                is Result.Success -> {
+private fun observeUI() {
+    viewModel.getAccount().observe(viewLifecycleOwner) {
+        when (it) {
+            is Result.Error -> {
 
-                    binding.apply {
+            }
+            Result.Loading -> {
 
-                        if (it.data.imageUrl != null) {
-                            if (getFile == null){
-                                with(profileImageView) { it.data.imageUrl?.let { it1 -> loadPhotoUrl(it1) } }
-                            }
-                        } else {
-                            binding.profileImageView.setImageResource(R.drawable.ic_avatar)
+            }
+            is Result.Success -> {
 
+                binding.apply {
+
+                    if (it.data.imageUrl != null) {
+                        if (getFile == null) {
+                            with(profileImageView) { it.data.imageUrl?.let { it1 -> loadPhotoUrl(it1) } }
                         }
-                    }
+                    } else {
+                        binding.profileImageView.setImageResource(R.drawable.ic_avatar)
 
+                    }
                 }
+
             }
         }
-        binding.icLogout.setOnClickListener {
-            LogoutProcess.execute(appLocalData, binding)
-        }
-
-
-
-
     }
+
 
 }
 
-
+}
