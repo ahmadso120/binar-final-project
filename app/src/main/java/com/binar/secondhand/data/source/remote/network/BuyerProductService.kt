fa1a4552dfa0aa912799bc7ec20c002b838bca3c
aditrizky <aditya10355@gmail.com>
2022-06-24 15:21:26 +0700

    update notification and edit account UI

--- app/src/main/java/com/binar/secondhand/data/source/remote/network/BuyerProductService.kt
+++ app/src/main/java/com/binar/secondhand/data/source/remote/network/BuyerProductService.kt
@@ -1,7 +1,9 @@
 package com.binar.secondhand.data.source.remote.network
 
 import com.binar.secondhand.data.source.remote.response.BuyerProductResponse
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface BuyerProductService {
--- app/src/main/java/com/binar/secondhand/data/source/remote/network/NotificationService.kt
+++ app/src/main/java/com/binar/secondhand/data/source/remote/network/NotificationService.kt
@@ -3,9 +3,16 @@ package com.binar.secondhand.data.source.remote.network
 import com.binar.secondhand.data.source.remote.response.NotificationResponseItem
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.Path
 
 interface NotificationService {
 
     @GET("notification")
     suspend fun getAllNotification(): Response<List<NotificationResponseItem>>
+
+    @PATCH("notification/{id}")
+    suspend fun patchNotification(
+        @Path("id") id : Int
+    ): Response<NotificationResponseItem>
 }
