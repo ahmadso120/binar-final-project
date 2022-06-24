134c7f77c3811be043b8dce385b6dbab4d4d90ba
Ahmad Sopian <sopianahmad120@gmail.com>
2022-06-24 10:48:41 +0700

    add date time formatter

--- app/src/main/java/com/binar/secondhand/utils/CommonExtension.kt
+++ app/src/main/java/com/binar/secondhand/utils/CommonExtension.kt
@@ -1,6 +1,9 @@
 package com.binar.secondhand.utils
 
 import timber.log.Timber
+import java.time.DateTimeException
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 
 inline val <reified T> T.TAG: String
     get() = T::class.java.simpleName
@@ -13,4 +16,15 @@ inline fun <reified T> T.loge(message: String) = Timber.tag(TAG).e(message)
 
 fun Int.currencyFormatter(): String {
     return String.format("%,d", this).replace(',', '.')
+}
+
+fun String.dateTimeFormatter(): String? {
+    return try {
+        val odt = OffsetDateTime.parse(this)
+        val dtf = DateTimeFormatter.ofPattern("dd MMM uuuu, mm:ss")
+        dtf.format(odt)
+    } catch (e: DateTimeException) {
+        loge("dateTimeFormatter => ${e.message}")
+        null
+    }
 }
