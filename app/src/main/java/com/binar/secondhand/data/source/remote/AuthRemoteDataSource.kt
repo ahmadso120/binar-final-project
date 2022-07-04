c06d2d4e31d27c36d8a2360a24347d773a730163
ImanuelRomuty <romuty16@gmail.com>
2022-07-04 21:47:27 +0700

    Add Preview

--- app/src/main/java/com/binar/secondhand/data/source/remote/AuthRemoteDataSource.kt
+++ app/src/main/java/com/binar/secondhand/data/source/remote/AuthRemoteDataSource.kt
@@ -3,12 +3,18 @@ package com.binar.secondhand.data.source.remote
 import com.binar.secondhand.data.source.remote.network.AuthService
 import com.binar.secondhand.data.source.remote.request.LoginRequest
 import com.binar.secondhand.data.source.remote.request.RegisterRequest
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 class AuthRemoteDataSource(
     private val authService: AuthService
 ) {
     suspend fun login(loginRequest: LoginRequest) =
         authService.login(loginRequest)
-    suspend fun register(registerRequest: RegisterRequest)=
-        authService.register(registerRequest)
+//    suspend fun register(registerRequest: RegisterRequest)=
+//        authService.register(registerRequest)
+suspend fun register(file: MultipartBody.Part?, partMap: Map<String, RequestBody>)=
+    authService.register(file,partMap)
 }
+
+
--- app/src/main/java/com/binar/secondhand/data/source/remote/request/RegisterRequest.kt
+++ app/src/main/java/com/binar/secondhand/data/source/remote/request/RegisterRequest.kt
@@ -1,10 +1,20 @@
 package com.binar.secondhand.data.source.remote.request
 
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.http.Part
+import retrofit2.http.PartMap
+
+//data class RegisterRequest(
+//    val full_name: String,
+//    val email: String,
+//    val password: String,
+//    val phone_number: String,
+//    val address :String,
+//    val city    : String
+//)
+
 data class RegisterRequest(
-    val full_name: String,
-    val email: String,
-    val password: String,
-    val phone_number: String,
-    val address :String,
-    val city    : String
+@Part val file : MultipartBody.Part?,
+@PartMap val partMap : Map<String,RequestBody>
 )
