cb4d851079e668b34d8549bf21b672fd08c93ea7
ImanuelRomuty <romuty16@gmail.com>
2022-07-06 11:54:04 +0700

    register name_email_password

--- app/src/main/java/com/binar/secondhand/data/source/remote/AuthRemoteDataSource.kt
+++ app/src/main/java/com/binar/secondhand/data/source/remote/AuthRemoteDataSource.kt
@@ -3,6 +3,8 @@ package com.binar.secondhand.data.source.remote
 import com.binar.secondhand.data.source.remote.network.AuthService
 import com.binar.secondhand.data.source.remote.request.LoginRequest
 import com.binar.secondhand.data.source.remote.request.RegisterRequest
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 class AuthRemoteDataSource(
     private val authService: AuthService
@@ -10,5 +12,7 @@ class AuthRemoteDataSource(
     suspend fun login(loginRequest: LoginRequest) =
         authService.login(loginRequest)
     suspend fun register(registerRequest: RegisterRequest)=
-        authService.register(registerRequest)
+    authService.register(registerRequest)
 }
+
+
--- app/src/main/java/com/binar/secondhand/data/source/remote/request/RegisterRequest.kt
+++ app/src/main/java/com/binar/secondhand/data/source/remote/request/RegisterRequest.kt
@@ -1,10 +1,12 @@
 package com.binar.secondhand.data.source.remote.request
 
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.http.Part
+import retrofit2.http.PartMap
+
 data class RegisterRequest(
     val full_name: String,
     val email: String,
     val password: String,
-    val phone_number: String,
-    val address :String,
-    val city    : String
 )
