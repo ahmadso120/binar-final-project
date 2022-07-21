d46c0c71daa520c5ec610851f7ac087c00011ac9
Ahmad Sopian <sopianahmad120@gmail.com>
2022-07-21 18:42:38 +0700

    Adjust the spacing view in account page

--- app/src/main/java/com/binar/secondhand/ui/account/AccountFragment.kt
+++ app/src/main/java/com/binar/secondhand/ui/account/AccountFragment.kt
@@ -14,6 +14,8 @@ import com.binar.secondhand.storage.AppLocalData
 import com.binar.secondhand.ui.account.editaccount.EditAccountViewModel
 import com.binar.secondhand.ui.search.SearchViewModel
 import com.binar.secondhand.utils.LogoutProcess
+import com.binar.secondhand.utils.getInitialsName
+import com.binar.secondhand.utils.logd
 import com.binar.secondhand.utils.ui.loadPhotoUrl
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -25,20 +27,18 @@ class AccountFragment : BaseFragment(R.layout.fragment_account) {
 
     private val viewModel by viewModel<EditAccountViewModel>()
 
-    private val searchViewModel by viewModel<SearchViewModel>()
-   
-
     private val appLocalData: AppLocalData by inject()
     private lateinit var builder: AlertDialog.Builder
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         observeUI()
-        var listView = binding.listview
-        var menuAccount: ArrayList<MenuAccount> = ArrayList()
+
+        val listView = binding.listview
+        val menuAccount: ArrayList<MenuAccount> = ArrayList()
         menuAccount.add(MenuAccount("Wishlist", R.drawable.ic_wishlist))
         menuAccount.add(MenuAccount("Daftar Order", R.drawable.ic_orders))
-        menuAccount.add(MenuAccount("Riwayat",R.drawable.ic_baseline_history_edu_24))
+        menuAccount.add(MenuAccount("Riwayat", R.drawable.ic_baseline_history_edu_24))
         menuAccount.add(MenuAccount("Ubah Akun", R.drawable.ic_fi_edit))
         menuAccount.add(MenuAccount("Ubah Password", R.drawable.ic_fi_settings))
         menuAccount.add(MenuAccount("Keluar", R.drawable.ic_fi_log_out))
@@ -51,12 +51,7 @@ class AccountFragment : BaseFragment(R.layout.fragment_account) {
                 4 -> {
                     navController.navigate(R.id.action_accountFragment_to_accountSettingFragment2)
                 }
-
-                
                 5 -> {
-//                    searchViewModel.deleteHistory()
-               
-                    
                     builder = AlertDialog.Builder(requireContext())
                     builder.setTitle("Keluar dari SecondHand")
                         .setMessage("Apakah anda ingin keluar ?")
@@ -76,13 +71,12 @@ class AccountFragment : BaseFragment(R.layout.fragment_account) {
                     findNavController().navigate(AccountFragmentDirections.actionAccountFragmentToBuyerOrderFragment())
                 }
 
-                2 ->{
+                2 -> {
                     findNavController().navigate(R.id.action_accountFragment_to_historyFragment)
                 }
-                 0 -> {
-                     findNavController().navigate(AccountFragmentDirections.actionAccountFragmentToWishlistFragment())
-                 }
-
+                0 -> {
+                    findNavController().navigate(AccountFragmentDirections.actionAccountFragmentToWishlistFragment())
+                }
             }
         }
     }
@@ -98,10 +92,10 @@ class AccountFragment : BaseFragment(R.layout.fragment_account) {
                 }
                 is Result.Success -> {
                     binding.apply {
-                        if (it.data.imageUrl != null) {
-                            profileImageView.loadPhotoUrl(it.data.imageUrl)
+                        if (it.data.imageUrl.isNullOrEmpty()) {
+                            initialsNameTv.text = it.data.fullName.getInitialsName()
                         } else {
-                            profileImageView.setImageResource(R.drawable.ic_avatar)
+                            profileImageView.loadPhotoUrl(it.data.imageUrl)
                         }
                     }
                 }
